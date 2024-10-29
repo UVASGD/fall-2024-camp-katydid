777300641d4e0184514fc0ade3901521435c2a4a
pharingWell <30706897+pharingWell@users.noreply.github.com>
2024-10-28 20:59:40 -0400

    Squashed commit of the following:
    commit 22b7647fcd5376f92ba36777095e9661aacd2280
    Merge: 1a23c30 30936f0
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Thu Oct 24 19:53:10 2024 -0400
    
        Merge branch 'arthur-branch' of https://github.com/UVASGD/fall-2024-camp-katydid into arthur-branch
    
    commit 1a23c30c3c5d98f64ab630614299d44ba48e27d3
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Thu Oct 24 19:52:29 2024 -0400
    
        mse changes
    
    commit 30936f09831cb7d2995ef185fb2faebb2ea3a2ca
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Mon Oct 21 20:04:51 2024 -0400
    
        adds to correct index now
    
    commit 18cf084c3680f72884a287cf28e4a25f7ccab56e
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Thu Oct 17 20:29:36 2024 -0400
    
        changed sorting order for ui elements so that inventory is drawn over everything else
    
    commit 1bd2b35fd2fd5ffaa0889240e67ce61fc3b6cd76
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Thu Oct 17 20:28:38 2024 -0400
    
        dialogue doesn't appear over inventory and you can no longer index through dialogue when inventory is open
    
    commit 31b3d61a00197922208a54f28692a0b465fc3196
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Thu Oct 17 20:07:23 2024 -0400
    
        mostly refactoring, added npc getter function to query current dialogue for quest/task implementation
    
    commit 4f3819aa8c1ad17af5d7496aa9cf97b745cf4a8e
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Sun Oct 13 11:13:34 2024 -0400
    
        npc script refactoritng
    
    commit e4df31eaa136d2befb848dc50f96cbf810a4b757
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Sun Oct 13 11:11:49 2024 -0400
    
        scene changes
    
    commit 95dc301fcf6593bfe35328125b2ac9bc6caf0921
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Sun Oct 13 11:10:53 2024 -0400
    
        implemented NPC giving items
    
        item type list must match dialogue list length. npc only gives items on first instance of that dialogue index
    
    commit 1de7de795dfe54a80d990dfc4e961e8d9b27ae26
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Sun Oct 13 09:29:06 2024 -0400
    
        commits from thursday
    
    commit bf1a1a3b74339ad6196b1a45b1ae6e27748537c7
    Merge: fcac249 633f2f0
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Thu Oct 10 20:13:39 2024 -0400
    
        Merge branch 'develop' into arthur-branch
    
    commit fcac2490cfbec6fc4b7b8f3a156b8818e841bae1
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Thu Oct 10 20:10:55 2024 -0400
    
        inventory fix
    
    commit 7d3706d96bdd5d39d1e09dfe6a0017504ee5b1e7
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Thu Oct 10 15:03:28 2024 -0400
    
        added Itemtype drop down to NPC
    
    commit 1d71fee90501e4574d88f7cfa87e923e04f3ef2d
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Thu Oct 10 12:31:19 2024 -0400
    
        refactoring stuff
    
    commit 4ad815918cfe6bae972f17471fc16fd0fbf5f96b
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Wed Oct 9 21:27:12 2024 -0400
    
        inventory system
    
    commit 41af9546559ef3aa3a7b5cf439d9322ffc2671e2
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Mon Oct 7 20:15:06 2024 -0400
    
        unity save changes
    
    commit b736663537de85670e123c46334cc241823bcb0b
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Mon Oct 7 20:14:21 2024 -0400
    
        halfway-done invetory system
    
        pushing so i can work on my desktop
    
    commit 8787b31a21b30f146f7823eee0cef10c8a98779a
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Thu Oct 3 20:56:41 2024 -0400
    
        scene changes
    
    commit 0a18aded6da221e377fec665f4c3f5bdbfd97e3c
    Author: arthuroker <okerarthur@gmail.com>
    Date:   Thu Oct 3 20:56:22 2024 -0400
    
        Dialogue arrow moves dynamically to npc that is talking

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,23 +6,28 @@ using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
     public bool moveLock = true;
+
+    private bool firstTimeInventoryOpen = true;
     
     //private Vector2 _input;
     private CharacterController _characterController;
     private float speed = 50f;
 
+    public Inventory inventory;
+
     [SerializeField] private SpriteRenderer body;
     [SerializeField] private SpriteRenderer hair;
-    [SerializeField] private SpriteRenderer hairColor;
-    [SerializeField] private SpriteRenderer eye;
-    [SerializeField] private SpriteRenderer eyeColor;
+    [SerializeField] private SpriteRenderer eyes;
+    [SerializeField] private SpriteRenderer mouth;
+    [SerializeField] private SpriteRenderer top;
 
     [SerializeField] private Sprite[] bodies;
     [SerializeField] private Sprite[] hairs;
-    [SerializeField] private Sprite[] hairColors;
-    [SerializeField] private Sprite[] eyes;
-    [SerializeField] private Sprite[] eyeColors;
+    [SerializeField] private Sprite[] eyePairs;
+    [SerializeField] private Sprite[] mouths;
+    [SerializeField] private Sprite[] tops;
 
+    [SerializeField] private UI_Inventory uI_Inventory;
     public enum flags { defaultFlag, //flag put on all dialogue
             testFlag1, 
             testFlag2,
@@ -35,12 +40,11 @@ public class Player : MonoBehaviour
     private void Start()
     {
         body.sprite = bodies[PlayerPrefs.GetInt("bodyIndex")];
-        int hairIndex = PlayerPrefs.GetInt("hairIndex");
-        hair.sprite = hairs[hairIndex];
-        hairColor.sprite = hairColors[(PlayerPrefs.GetInt("hairCIndex")) + (hairIndex * 5)];
-        int eyeIndex = PlayerPrefs.GetInt("eyeIndex");
-        eye.sprite = eyes[eyeIndex];
-        eyeColor.sprite = eyeColors[(PlayerPrefs.GetInt("eyeCIndex")) + (eyeIndex * 5)];
+        hair.sprite = hairs[PlayerPrefs.GetInt("hairIndex")];
+        eyes.sprite = eyePairs[PlayerPrefs.GetInt("eyeIndex")];
+        mouth.sprite = mouths[PlayerPrefs.GetInt("mouthIndex")];
+        top.sprite = tops[PlayerPrefs.GetInt("topIndex")];
+        inventory = new Inventory();
 
         _characterController = GetComponent<CharacterController>();
 
@@ -49,6 +53,13 @@ public class Player : MonoBehaviour
         for(int i=0; i < data.flags.Length; i++)
         {
             dialogueFlags.Add((flags)data.flags[i]);
+        }
+
+        for(int i=0; i < data.inventoryItems.Length; i++)
+        {
+            InventoryItem inventoryItemData = new InventoryItem();
+            inventoryItemData.itemType = (InventoryItem.ItemType) data.inventoryItems[i];
+            inventory.AddInventoryItem(inventoryItemData);
         }
         
         transform.position = new Vector3(data.playerPosition[0], data.playerPosition[1], data.playerPosition[2]);
@@ -63,6 +74,16 @@ public class Player : MonoBehaviour
         {
             Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             _characterController.Move(move * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.I) && firstTimeInventoryOpen)
+        {
+            firstTimeInventoryOpen = false;
+            uI_Inventory.SetInventory(inventory);
+
+        } else if (Input.GetKeyDown(KeyCode.I))
+        {
+            uI_Inventory.RefreshInventoryItems();
         }
     }
 
@@ -83,5 +104,10 @@ public class Player : MonoBehaviour
             output += " ";
         }
         Debug.Log(output);
+    }
+
+    public void AddToInventory(InventoryItem inventoryItem)
+    {
+        inventory.AddInventoryItem(inventoryItem);
     }
 }
